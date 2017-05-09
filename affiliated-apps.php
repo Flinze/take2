0de84b3b23bd5ea60f0ee939d92896605361d530
Felix Lin <blazingfelix@gmail.com>
2017-05-08 21:54:07 -0700

    Moved stylesheets and scripts from affiliated-apps.php to header.php and footer.php

--- affiliated-apps.php
+++ affiliated-apps.php
@@ -1,80 +1,70 @@
 <!DOCTYPE HTML>
 <html lang="en">
-    <head>
-        <title>Take2 - About</title>
-        <meta charset="utf-8" />
+  <head>
+    <title>Take2 - About</title>
+    <meta charset="utf-8" />
+  </head>
 
-        <!-- Bootstrap -->
-        <script src="assets/jquery/jquery-3.2.1.min.js"></script>
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
-        <script src="assets/bootstrap/js/bootstrap.js"></script>
+  <body>
+		<!-- Wrapper -->
+		<div id="wrapper" class="container">
+      <div class="row">
+  			<!-- Header -->
+        <?php include('header.php') ?>
 
-        <!-- Stylesheets -->
-        <link rel="stylesheet" href="assets/style/base.css" />
-    </head>
+  			<!-- Main Content -->
+  			<main class="col-xs-10 col-lg-8 col-xs-offset-1 col-lg-offset-2">
 
-    <body>
-  		<!-- Wrapper -->
-  		<div id="wrapper">
-        <div class="container">
+  				<div class="major-title">
+  					Affiliated Apps
+  				</div>
+
+          <!-- Affiliated apps content -->
           <div class="row">
-      			<!-- Header -->
-            <?php include('header.php') ?>
-
-      			<!-- Main Content -->
-      			<main class="col-xs-10 col-lg-8 col-xs-offset-1 col-lg-offset-2">
-
-
-      				<div class="major-title">
-      					Affiliated Apps
-      				</div>
-
-              <!-- Affiliated apps content -->
-              <div class="row">
-                <div class="col-xs-8 col-xs-offset-2">
-                  <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                      <h3>Thumbnail label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                    </div>
-                  </div>
+            <div class="col-xs-8 col-xs-offset-2">
+              <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
                 </div>
               </div>
-
-              <div class="row">
-                <div class="col-xs-8 col-xs-offset-2">
-                  <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                      <h3>Thumbnail label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-xs-8 col-xs-offset-2">
-                  <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                      <h3>Thumbnail label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-      			</main>
-
-      			<!-- Footer -->
-            <?php include('footer.php'); ?>
-
+            </div>
           </div>
-        </div>
-  		</div>
-    </body>
+
+          <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+              <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+              <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+  			</main>
+
+  			<!-- Footer -->
+        <?php include('footer.php'); ?>
+
+
+      </div>
+		</div>
+  </body>
 </html>
