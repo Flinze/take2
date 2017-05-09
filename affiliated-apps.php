c005d14468e414ae5066869ba8fe664f298b0870
Felix Lin <blazingfelix@gmail.com>
2017-05-08 17:36:53 -0700

    Fixed non-responsive affiliated apps page

--- affiliated-apps.php
+++ affiliated-apps.php
@@ -15,8 +15,9 @@
 
     <body>
 		<!-- Wrapper -->
-		<div id="wrapper" class="container">
-
+		<div id="wrapper">
+      <div class="container">
+        <div class="row">
 			<!-- Header -->
       <?php include('header.php') ?>
 
@@ -29,121 +30,51 @@
 				</div>
 
         <!-- Top row -->
-
         <div class="row">
-          <div class="col-xs-4 col-md-4">
+          <div class="col-xs-8 col-xs-offset-2">
             <div class="thumbnail">
               <img src="..." alt="...">
               <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
               </div>
             </div>
           </div>
         </div>
 
-        <!-- Middle row -->
-
         <div class="row">
-          <div class="col-xs-4 col-md-4">
+          <div class="col-xs-8 col-xs-offset-2">
             <div class="thumbnail">
               <img src="..." alt="...">
               <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
               </div>
             </div>
           </div>
         </div>
 
-        <!-- Bottom row -->
         <div class="row">
-          <div class="col-xs-4 col-md-4">
+          <div class="col-xs-8 col-xs-offset-2">
             <div class="thumbnail">
               <img src="..." alt="...">
               <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xs-4 col-md-4">
-            <div class="thumbnail">
-              <img src="..." alt="...">
-              <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
               </div>
             </div>
           </div>
         </div>
-
-
 			</main>
 
 			<!-- Footer -->
       <?php include('footer.php'); ?>
 
+    </div>
+    </div>
 		</div>
     </body>
 </html>
