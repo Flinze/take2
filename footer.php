4bd0b6ab278dd3d6844a368fc6ebfa44c167d622
Felix Lin <blazingfelix@gmail.com>
2017-05-10 16:08:04 -0700

    Fixed nav bar issues

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 </footer>
 
 <!-- Scripts -->
-<script src="assets/jquery/jquery-3.2.1.min.js"></script>
+<script src="assets/jquery/jquery.min.js"></script>
 <script src='assets/js/select2.full.js'></script>
 <script src="assets/js/skel.min.js"></script>
 <script src="assets/js/base.js"></script>
