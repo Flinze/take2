a91cbda8e9f7e769457e7cf3f4ee8df6a45b5469
Felix Lin <blazingfelix@gmail.com>
2017-05-10 14:07:27 -0700

    Added linked images to the footer, need to fix for browser

--- footer.php
+++ footer.php
@@ -1,26 +1,22 @@
 <!-- Footer -->
-<div class="container">
-  <div class="row">
-    <div>
+<footer>
+    <div class="row">
+        <div class="col-xs-offset-2 col-xs-8">
+          <ul class="footer-links">
+            <li><a href="index.php"><img src="img/footer/home.png" alt="home page"></a></li>
+            <li><a href="recipes.php"><img src="img/footer/recipes.png" alt="recipes page"></a></li>
+            <li><a href="about.php"><img src="img/footer/aboutus.png" alt="about us page"></a></li>
+            <li><a href="affiliated-apps.php"><img src="img/footer/affiliated_apps.png" alt="affiliated apps page"></a></li>
+          </ul>
+        </div>
+    </div>
 
-      <div class="col-xs-offset-2 col-xs-2">
-        <ul id="footer-links">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="recipes.php">Recipes</a></li>
-          <li><a href="about.php">About</a></li>
-          <li><a href="affiliated-apps.php">Affiliated Apps</a></li>
-        </ul>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8 ">
+        Copyright &copy; 2017 Take2 | Designed by TEAM
       </div>
-
-
     </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-offset-4 col-xs-8 ">
-      Copyright &copy; 2017 Take2 | Designed by TEAM
-    </div>
-  </div>
-</div>
+</footer>
 
 <!-- Scripts -->
 <script src="assets/jquery/jquery-3.2.1.min.js"></script>
