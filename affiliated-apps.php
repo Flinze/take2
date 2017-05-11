3f69e6c53c35ec46458ae3d759c9058047e39554
andrewle-bc <andrewle.bc@gmail.com>
2017-05-11 00:54:42 -0700

    affiliated app page is now a nice looking grid, also editted logos

--- affiliated-apps.php
+++ affiliated-apps.php
@@ -20,8 +20,8 @@
 		</div>
 
 		<!-- Affiliated apps content -->
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+		
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
 				<div class="logo-container">
 				
 					<!-- Affiliated-app Logo -->
@@ -37,10 +37,9 @@
 							<a class="glyphicon glyphicon-plus" href="#">INFO</a>
 						</div>
 					</div>
-					
 				</div>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
 				<div class="logo-container">
 				
 					<!-- Affiliated-app Logo -->
@@ -58,9 +57,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
 				<div class="logo-container">
 				
 					<!-- Affiliated-app Logo -->
@@ -78,7 +75,61 @@
 					</div>
 				</div>
 			</div>
-		</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+				<div class="logo-container">
+				
+					<!-- Affiliated-app Logo -->
+					<img src="img/affiliated-apps/freshness_logo.png" class="img-responsive" alt="freshness">
+					
+					<!-- Affiliated-app Logo Slider -->
+					<div class="caption">
+						<div class="blur">
+						</div>
+						<div class="caption-text">
+							<h3 class="title">Freshness</h3>
+							<p class="app-description">An informative app that provides food preservation methods to help food stay as fresh as possible</p>
+							<a class="glyphicon glyphicon-plus" href="#">INFO</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+				<div class="logo-container">
+				
+					<!-- Affiliated-app Logo -->
+					<img src="img/affiliated-apps/F2E_logo.png" class="img-responsive" alt="F2E">
+					
+					<!-- Affiliated-app Logo Slider -->
+					<div class="caption">
+						<div class="blur">
+						</div>
+						<div class="caption-text">
+							<h3 class="title">F2E</h3>
+							<p class="app-description">A food-sharing app that allows users to connect and easily share leftover ingredients and meals</p>
+							<a class="glyphicon glyphicon-plus" href="#">INFO</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+				<div class="logo-container">
+				
+					<!-- Affiliated-app Logo -->
+					<img src="img/affiliated-apps/Eco_logo.png" class="img-responsive" alt="Eco-Logo">
+					
+					<!-- Affiliated-app Logo Slider -->
+					<div class="caption">
+						<div class="blur">
+						</div>
+						<div class="caption-text">
+							<h3 class="title">EcoList</h3>
+							<p class="app-description">A grocery list and meal planning app that suggests recipes and tracks expiration dates.</p>
+							<a class="glyphicon glyphicon-plus" href="#">INFO</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		
 	</main>
 	
 		<!-- Footer -->
