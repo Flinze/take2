4a9f4eaddc2cc9331f4a492f1b24ab73600c5831
andrewle-bc <andrewle.bc@gmail.com>
2017-05-17 11:08:04 -0700

    removed 3 aff app
    removed ecolist, freshness, wasted
    kept f2e, fuber, grocery buddy

--- affiliated-apps.php
+++ affiliated-apps.php
@@ -25,24 +25,6 @@
 				<div class="logo-container">
 
 					<!-- Affiliated-app Logo -->
-					<img src="img/affiliated-apps/freshness_logo.png" class="img-responsive" alt="freshness">
-
-					<!-- Affiliated-app Logo Slider -->
-					<div class="caption">
-						<div class="blur">
-						</div>
-						<div class="caption-text">
-							<h3 class="title">Freshness</h3>
-							<p class="app-description">An informative app that provides food preservation methods to help food stay as fresh as possible</p>
-							<a class="btn btn-info" href="https://freshness.000webhostapp.com/index.html">View Page</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="logo-container">
-
-					<!-- Affiliated-app Logo -->
 					<img src="img/affiliated-apps/F2E_logo.png" class="img-responsive" alt="F2E">
 
 					<!-- Affiliated-app Logo Slider -->
@@ -53,42 +35,6 @@
 							<h3 class="title">F2E</h3>
 							<p class="app-description">A food-sharing app that allows users to connect and easily share leftover ingredients and meals</p>
 							<a class="btn btn-info" href="http://students.bcitdev.com/A01005177/eugene/main.html">View Page</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="logo-container">
-
-					<!-- Affiliated-app Logo -->
-					<img src="img/affiliated-apps/Eco_logo.png" class="img-responsive" alt="Eco">
-
-					<!-- Affiliated-app Logo Slider -->
-					<div class="caption">
-						<div class="blur">
-						</div>
-						<div class="caption-text">
-							<h3 class="title">EcoList</h3>
-							<p class="app-description">A grocery list and meal planning app that suggests recipes and tracks expiration dates.</p>
-							<a class="btn btn-info" href="http://www.eco-list.ca">View Page</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="logo-container">
-
-					<!-- Affiliated-app Logo -->
-					<img src="img/affiliated-apps/wasted_logo.png" class="img-responsive" alt="Wasted">
-
-					<!-- Affiliated-app Logo Slider -->
-					<div class="caption">
-						<div class="blur">
-						</div>
-						<div class="caption-text">
-							<h3 class="title">Wasted</h3>
-							<p class="app-description">Web app that tracks what food you have on hand and reminds you when it's about to go bad. Users update a list of on hand items with  their expiry dates and app pushes a reminder.</p>
-							<a class="btn btn-info" href="http://students.bcitdev.com/A00989687/forAffiliates/WASTED.html">View Page</a>
 						</div>
 					</div>
 				</div>
