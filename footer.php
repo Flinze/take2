7be475bff11cc1387712bdfaef66f6d4ae75a7eb
Jeff Choy <choyjef@gmail.com>
2017-05-18 11:16:37 -0700

    moved embedded scripts and style to external

--- footer.php
+++ footer.php
@@ -23,8 +23,7 @@
 <script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
 <script src="assets/jquery/jquery-3.2.1.min.js"></script>
 <script src="assets/jquery/jquery.touchSwipe.min.js"></script>
-<!-- <script src="assets/jquery/jquery-migrate-3.0.0.min.js"></script>
-<script src="assets/jquery/jquery.mobile-1.4.5.min.js"></script> -->
+<script src="assets/js/recipeList.js"></script>
 <script src='assets/js/select2.full.js'></script>
 <script src="assets/js/skel.min.js"></script>
 <script src="assets/js/base.js"></script>
--- recipes.php
+++ recipes.php
@@ -3,36 +3,6 @@
 <head>
 	<title>Take2 - Recipe List</title>
 	<meta charset="utf-8">
-    <script>
-        function recipeListModal(x){
-            var ingArr = x.split("-");
-            var id = x.slice(x.length - 1, x.length);
-            ingArr[1] = ingArr[1].substr(0, (ingArr[1].length - 1));
-
-            populateRecipeModal(ingArr[0], ingArr[1], id);
-            $('#recipe-modal').modal('show');
-        }
-
-        function filter(restriction){
-            restriction = restriction.toLowerCase();
-            $('.recList').hide();
-            $(("." +restriction)).show();
-        }
-
-        function recipeFilter(){
-            var restr = $(":checked");
-            var sift = ""
-            for (var i = 0; i < restr.length; i++){
-                sift += "." + restr[i].name;
-            }
-            $('.recList').hide();
-            if (restr.length == 0) {
-                $('.recList').show();
-            } else {
-                $('.recList').filter(sift).show();
-            }
-        }
-    </script>
 </head>
 
 <body>
