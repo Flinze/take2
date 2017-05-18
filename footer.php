0842983a55c4c76cb691b709985da6e0cdba7cf8
Felix Lin <blazingfelix@gmail.com>
2017-05-18 11:11:50 -0700

    Added individual classes for each footer icon

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 	<!-- Footer Icons -->
 	<ul class="footer-links">
 
-		<li class="footer-icon"><a href="index.php"><img class="icon" src="img/footer/home.png" alt="home page"></a></li>
-		<li class="footer-icon"><a href="recipes.php"><img class ="icon" src="img/footer/recipes.png" alt="recipes page"></a></li>
-		<li class="footer-icon"><a href="about.php"><img class="icon" src="img/footer/aboutus.png" alt="about us page"></a></li>
-		<li class="footer-icon"><a href="affiliated-apps.php"><img class="icon" src="img/footer/affiliated_apps.png" alt="affiliated apps page"></a></li>
-		<li class="footer-icon"><a href="contact.php"><img class="icon" src="img/footer/contact.png" alt="contact page"></a></li>
+		<li class="footer-icon"><a href="index.php"><img class="icon" class="home-icon" src="img/footer/home.png" alt="home page"></a></li>
+		<li class="footer-icon"><a href="recipes.php"><img class ="icon" class="recipe-icon" src="img/footer/recipes.png" alt="recipes page"></a></li>
+		<li class="footer-icon"><a href="about.php"><img class="icon" class="about-icon" src="img/footer/aboutus.png" alt="about us page"></a></li>
+		<li class="footer-icon"><a href="affiliated-apps.php"><img class="icon" class="affiliated-icon" src="img/footer/affiliated_apps.png" alt="affiliated apps page"></a></li>
+		<li class="footer-icon"><a href="contact.php"><img class="icon" class="contact-icon" src="img/footer/contact.png" alt="contact page"></a></li>
 
 
 	</ul>
