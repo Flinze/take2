ba9c030b4dcc9b6a1a96120a53b3d5536c4d8413
andrewle-bc <andrewle.bc@gmail.com>
2017-05-23 18:52:53 -0700

    format about page

--- about.php
+++ about.php
@@ -19,7 +19,6 @@
   		<!-- Main Content -->
 		<main class="col-xs-10 col-xs-offset-1 col-lg-8 col-lg-offset-2">
 
-
 		<div class="major-title">
 			About Us
 		</div>
@@ -28,30 +27,39 @@
 			<img id="team-photo" src="img/about/group.jpg" alt="Group Photo" onclick="imageChange()">
 		</div>
 		
-		<blockquote>
+		<div id="flock-intro">
 			Segal Au - Jenny Ly - Jeff Choy - Andrew Le - Felix Lin
 			<br>
 			<br>
-			<i>"Must have fun"</i>
-		</blockquote>
-
-		<div class="major-title">
-			<i class="fa fa-male"></i><i class="fa fa-female"></i>The Flock<i class="fa fa-male"></i><i class="fa fa-male"></i><i class="fa fa-male"></i>
+			<i>
+			"There was an idea, called the <b>Flock</b> Initiative. The idea was to bring together a group of remarkable people, to see if they could become something more, see if they can work together when we needed them to, and overcome challenges too great for one person."
+			</i>
 		</div>
 
-		<p>
-		There was an idea, called the Flock Initiative. The idea was to bring together of remarkable people, see if they could become something more, see if they can work together when we needed them to. To fight the battles that we never could.
+		<div class="major-title">
+			<i class="fa fa-male"></i><i class="fa fa-female"></i> The Flock <i class="fa fa-male"></i><i class="fa fa-male"></i><i class="fa fa-male"></i>
+		</div>
+
+		<p id="flock-bio">
+		&emsp;&emsp;The Flock is made up of level one students in BCIT's Computer System Technology program.
 		</p>
+		
+		<br>
+		
+		<!-- 30s pitch easter egg -->
 		<div class="vid-title-container">
 			<div class="major-title">
-				30 Second Pitch
+				<i class="fa fa-hourglass"></i> 30 Second Pitch <i class="fa fa-hourglass-o"></i>
 			</div>
-			<p> Oh, you're still here! You must have a lot of spare time. Well then, take a look at our 30 second pitch.</p>
+			
+			<h4>Oh! You're still here after 30 seconds. Please check out our 30 second video pitch!</h4><br>
+			
 			<div class="vid-container">
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/Q-hQLBQn9Lw" frameborder="0" allowfullscreen></iframe>
 			</div>
 			<br>
 		</div>
+		
 	</main>
 
 	<!-- Footer -->
